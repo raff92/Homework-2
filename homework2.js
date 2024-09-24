57d2cab94ae1b19c844cb6cf3f4f16891ab427be
raff92 <macho.vaganyan75@mail.ru>
2024-09-24 19:29:42 +0300

    Add files via upload

---
+++ homework2.js
@@ -0,0 +1,130 @@
+// 1 Задание
+
+
+let gg = prompt ( " Напишите У  " )
+let baby = gg + 12 / 12 === 1  // 1
+let toddler = gg + 24 / 12 === 2 // 2
+let child = gg + 108 / 12 === 9 // 9 
+let teenager = gg + 168 / 12 === 15 // 14
+let adult = gg + 444 / 12 === 37 // 37
+
+let min = ""
+
+    min = gg + " меня есть 4 внука и 1 брат, " 
+ if (!baby) {
+    min += "Первому внуку 6 месяцев, " 
+} if (!toddler) {
+    min += "Второму внуку год, " 
+} if (!child) {
+    min += "Третьяму внуку 9 лет, "
+} if (!teenager) {
+    min += "Четвертому уже 14 лет, "
+} if (!adult) { 
+    min += "а брату моему уже 37 лет "
+}
+    
+// 2 Задание
+
+let a = 44
+let b = 27
+let c = 134 
+
+console.log (b, a, c)
+
+// 
+let a = -53
+let b = 41
+let c = -90
+
+console.log (c,a,b)
+
+// или можно
+
+    
+let play = +prompt ( " Напишите 1 " ) 
+let a = play + 5 
+let b = play + 27 
+let c = play + 134 
+let hp = ""
+
+if (a > b && b > c && c < a) {
+    hp = play + " Неверное значение " 
+} else {
+hp = play + ""
+} 
+ if ( a < b ) {
+     hp  += " 5, "
+ } if ( b < c ) {
+     hp += "27 " 
+ } if ( c > b ) {
+     hp += "134 "
+ }
+    
+//
+
+let play = +prompt ( " Напишите -99, " ) 
+let a = play + -54
+let b = play + -29
+let c = play + 157
+let hp = ""
+
+if (a > b && b > c && c < a) {
+    hp = play + " Неверное значение " 
+} else {
+hp = play + ""
+} 
+ if ( a < b ) {
+     hp  += " -54, "
+ } if ( b < c ) {
+     hp += "-29, " 
+ } if ( c > b ) {
+     hp += "157 "
+ }
+    
+// 3 Задание
+
+let num1 = 0 
+let num2 = -10
+let nume = 20 
+console.log("0")
+
+// 
+
+let num1 = -7 
+let num2 = -30 
+let num3 = 40
+console.log("+")
+
+//
+
+let num1 = -40
+let num2 = 5
+let num3 = 10
+console.log("-")
+
+// 4 Задание 
+
+
+var n = +prompt();
+var i = 0;
+var j = 0;
+if (!n % 2) {
+if (!Math.floor(n/10)) {
+    i += 1; 
+} if (!n % 3 ) { 
+(!n % 10) } 
+    j += 1; 
+}
+
+// 5 Задание 
+
+console.log ("9, 12, 1") 
+
+consol.log ("9,9,0")
+
+console.log ("4,2,6")
+
+
+
+
+    
